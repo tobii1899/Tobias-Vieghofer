a3c41e206c23be74373acf991b4175e2503ba3e6
Tobias <t.vieghofer@students.htl-leonding.ac.at>
2025-12-16 20:08:23 +0100

    updated portfolio completly

---
+++ scroll.js
@@ -0,0 +1,14 @@
+const sections = document.querySelectorAll(".section");
+
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+        });
+    },
+    { threshold: 0.15 }
+);
+
+sections.forEach(section => observer.observe(section));
